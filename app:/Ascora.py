a7224e62acabcf060dedcec8de0e648180f518c3
pulkitavijay-png <pulkitavijay@gmail.com>
2026-04-12 16:29:10 +0530

    Teacher's Dashboard

--- app:/Ascora.py
+++ app:/Ascora.py
@@ -348,4 +348,36 @@ elif role == "Teacher Assistant":
             st.session_state.active_topic = topic
             st.session_state.is_live = True
             st.session_state.live_subject = subject
+            with lec_col:
+                with st.container(border=True):
+                    t1, t2 = st.tabs(["🔴 Live Stream","📜 Transcript"])
+
+                    with t1:
+                        if st.session_state.is_live and st.session_state.live_subject == st.session_state.active_sub:
+                            st.success("🔴 LIVE CLASS")
+                            st.subheader(f"Topic: {st.session_state.active_topic}")
+
+                            col1, col2 = st.columns([1, 2])
+
+                            with col1:
+                                st.image(char, use_container_width=True)
+
+                            with col2:
+                                st.markdown("### 🎙️ Ascora Speaking...")
+                                audio_file = open("lecture.mp3", "rb")
+                                audio_bytes = audio_file.read()
+                                st.audio(audio_bytes, format="audio/mp3")
+
+                            st.divider()
+                            st.info("💡 Tip: You can ask doubts in the chat box below!")
+
+                        else:
+                            st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+                            st.caption("Waiting for the Facilitator to start the AI Robot session...")
+
+
+                    with t2:
+                        if st.session_state.is_live:
+                            st.write("Live Transcript Feed...")
+                            st.write(st.session_state.current_lecture)
            
